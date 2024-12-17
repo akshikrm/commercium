0318b90d539f7f652d8a35907686ed89710d6e2b
Akshay Krishna <akshikrm@gmail.com>
2024-12-17 13:36:59 +0530

    feat: Add my orders view
    References: #15

--- web/config.ts
+++ web/config.ts
@@ -1,2 +1,3 @@
 export const BASE_URL = "http://localhost:5234"
 export const BASE_URL_FILE = "http://localhost:8080"
+export const DATE_VIEW_FORMAT = "YYYY MMM DD"
--- web/src/handlers/purchase.ts
+++ web/src/handlers/purchase.ts
@@ -1,18 +1,6 @@
 import server from "@/utils/server"
 import { AxiosResponse } from "axios"
 
-type Purchases = {
-    id: number
-    purchase_price: number
-    order_id: string
-    product: {
-        id: number
-        name: string
-        price: number
-    }
-    created_at: string
-}
-
 export const getPurcahses = async (): Promise<Purchases[]> => {
     try {
         const { data } = await server.get("/purchase")
