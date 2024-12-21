768a638b4bab398134690893e84703fe78e87515
Akshay Krishna <akshikrm@gmail.com>
2024-12-21 15:31:35 +0530

    feat: Add create price when product is added

--- api/pkg/services/product.go
+++ api/pkg/services/product.go
@@ -2,10 +2,13 @@ package services
 
 import (
 	"akshidas/e-com/pkg/types"
+	"akshidas/e-com/pkg/utils"
 	"context"
 	"fmt"
+	"log"
 	"net/url"
 	"os"
+	"strconv"
 
 	"github.com/PaddleHQ/paddle-go-sdk"
 )
@@ -26,20 +29,65 @@ func (r *ProductService) Get(filter url.Values) ([]*types.ProductsList, error) {
 	return r.productModel.GetAll(filter)
 }
 
-func (r *ProductService) Create(newProduct *types.CreateNewProduct) error {
-	ctx := context.Background()
+func connectToPaddle() (*paddle.SDK, error) {
 	paddle_key := os.Getenv("PADDLE_API_KEY")
 	client, err := paddle.New(paddle_key, paddle.WithBaseURL(paddle.SandboxBaseURL))
-	pProduct, err := client.CreateProduct(ctx, &paddle.CreateProductRequest{
-		Name:        newProduct.Name,
-		TaxCategory: paddle.TaxCategoryStandard,
-		Description: &newProduct.Description,
-	})
+
 	if err != nil {
-		return err
+		log.Printf("failed to connect to paddle due to %s", err)
+		return nil, utils.ServerError
+	}
+	return client, nil
+}
+
+func (r *ProductService) addProductToPaddle(newProduct *types.CreateNewProduct) error {
+	ctx := context.Background()
+	client, err := connectToPaddle()
+
+	if err != nil {
+		log.Printf("failed to connect to paddle due to %s", err)
+		return utils.ServerError
 	}
 
-	newProduct.ProductID = pProduct.ID
+	product, err := client.CreateProduct(ctx, &paddle.CreateProductRequest{
+		Name:        newProduct.Name,
+		Description: &newProduct.Description,
+		TaxCategory: paddle.TaxCategoryStandard,
+	})
+
+	if err != nil {
+		log.Printf("failed to add product to paddle due to %s", err)
+		return utils.ServerError
+	}
+
+	price, err := client.CreatePrice(ctx, &paddle.CreatePriceRequest{
+		ProductID: product.ID,
+		UnitPrice: paddle.Money{
+			Amount:       strconv.Itoa(int(newProduct.Price)),
+			CurrencyCode: paddle.CurrencyCodeINR,
+		},
+		Description: "Main Price",
+	})
+
+	if err != nil {
+		log.Printf("failed to add price to product due to %s", err)
+		return utils.ServerError
+	}
+
+	newProduct.ProductID = product.ID
+	if price, err := strconv.Atoi(price.UnitPrice.Amount); err != nil {
+		log.Printf("failed to add price to product due to %s", err)
+		return utils.ServerError
+	} else {
+		newProduct.Price = uint(price)
+	}
+	return nil
+}
+
+func (r *ProductService) Create(newProduct *types.CreateNewProduct) error {
+	if err := r.addProductToPaddle(newProduct); err != nil {
+		return err
+	}
 	product, err := r.productModel.Create(newProduct)
 	fmt.Println(product.Name)
 	if err != nil {
