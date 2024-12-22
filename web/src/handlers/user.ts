011325d4f4b47784f3af3e12e7d8747efe259e18
Akshay Krishna <akshikrm@gmail.com>
2024-12-22 08:43:35 +0530

    Add role to profile

--- web/src/handlers/user.ts
+++ web/src/handlers/user.ts
@@ -1,10 +1,14 @@
 import server from "@utils/server"
+import { decodeJWT, getToken } from "@utils/session"
 import { AxiosResponse } from "axios"
 
 export const profile = async (): Promise<Profile> => {
     try {
         const { data } = await server.get("/profile")
-        return data.data
+        const jwt = getToken()
+        const decoded = decodeJWT(jwt)
+        const profile: Profile = { ...data.data, role: decoded.role }
+        return profile
     } catch (err) {
         const { data } = err as AxiosResponse
         console.error(data)
--- web/src/store/types.d.ts
+++ web/src/store/types.d.ts
@@ -2,4 +2,7 @@ type Profile = {
     role: Role | null
     first_name: string
     last_name: string
+    email: string
+    customer_id: string
+    phone_number: string
 }
