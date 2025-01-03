fd8ca05574fcb29650aaf8d655dfdc320dd27f14
Akshay Krishna <akshikrm@gmail.com>
2025-01-03 21:23:19 +0530

    refactor: removed some comments

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"akshidas/e-com/pkg/app"
 	"akshidas/e-com/pkg/repository"
 	"akshidas/e-com/pkg/services"
-	"flag"
 	"log"
 
 	"github.com/joho/godotenv"
@@ -14,22 +13,9 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	sync := flag.Bool("sync", false, "seed database")
-	flag.Parse()
 
 	store := repository.New()
-
-	if *sync {
-		paddlePayment := new(services.PaddlePayment)
-		if err := paddlePayment.Init(); err != nil {
-			panic(err)
-		}
-		paddlePayment.SyncPrice(store)
-		return
-	}
-
 	services := services.New(store)
-
 	server := app.New(":5324", services)
 
 	server.RegisterRoutes(app.UserHandler)
@@ -37,5 +23,6 @@ func main() {
 	server.RegisterRoutes(app.ProductCategoryHandler)
 	server.RegisterRoutes(app.CartHandler)
 	server.RegisterRoutes(app.PurchaseHandler)
+
 	server.Run()
 }
